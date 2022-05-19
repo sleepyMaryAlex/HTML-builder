4e7dde0f90d051c89e8536c1b1a1f180d110fd0b
Maria Guchkova <mariaguchkova1@gmail.com>
2022-05-19 17:06:42 +0300

    feat: implement second task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,12 @@
-const fs = require("fs");
-const path = require("node:path");
+const fs = require('fs');
+const path = require('path');
 async function createStream() {
-  let data = "";
-  const rs = fs.createReadStream(path.resolve("01-read-file", "text.txt"));
-  rs.on("data", (chunk) => {
+  let data = '';
+  const rs = fs.createReadStream(path.resolve('01-read-file', 'text.txt'));
+  rs.on('data', (chunk) => {
     data += chunk.toString();
   });
-  rs.on("end", () => {
+  rs.on('end', () => {
     console.log(data);
   });
 }
---
+++ 02-write-file/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const process = require('process');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function fileHandler() {
+  fs.open('02-write-file/text.txt', 'a+', textHandler);
+}
+fileHandler();
+
+let fileContent = '';
+let message = 'Hello, write some text, please!\n';
+function textHandler() {
+  rl.question(message, (answer) => {
+    if (answer !== 'exit') {
+      message = '';
+      fs.writeFile('02-write-file/text.txt', fileContent + answer, (err) => {
+        if (err) throw err;
+        fileContent += answer + '\n';
+      });
+      textHandler();
+    } else {
+      console.log('Thank you :) Have a nice day!');
+      rl.close();
+    }
+  });
+}
+
+rl.on('SIGINT', () => {
+  console.log('Thank you :) Have a nice day!');
+  rl.close();
+});
