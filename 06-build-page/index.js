11db6ea699c0ea670a50407f495ba09442f24fa4
Maria Guchkova <mariaguchkova1@gmail.com>
2022-05-23 14:55:18 +0300

    refactor: add async functions for sixth task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,97 +1,66 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 // create folder project-dist
 
-fs.mkdir(
-  path.resolve("06-build-page", "project-dist"),
-  { recursive: true },
-  (err) => {
-    if (err) throw err;
-    console.log("Created successfully");
-  }
-);
+async function createOutputFolder() {
+  const folderPath = path.resolve('06-build-page', 'project-dist');
+  await fs.promises.mkdir(folderPath, { recursive: true });
+}
 
 // check if index.html exists and remove
 
-fs.access(path.resolve("06-build-page", "project-dist", "index.html"), (err) => {
-  if (!err) {
-    fs.unlink(
-      path.resolve("06-build-page", "project-dist", "index.html"),
-      (err) => {
-        if (err) throw err;
-      }
-    );
+async function removeFile(file) {
+  const filePath = path.resolve('06-build-page', 'project-dist', file);
+  const stat = await fs.promises.stat(filePath).catch(() => null);
+  if (stat) {
+    await fs.promises.unlink(filePath);
   }
-});
+}
 
 // add index.html
 
 async function createHTML() {
-  let template = await fs.promises.readFile(
-    path.resolve("06-build-page", "template.html"),
-    "binary"
-  );
+  const templatePath = path.resolve('06-build-page', 'template.html');
+  let template = await fs.promises.readFile(templatePath, 'binary');
 
-  let components = await fs.promises.readdir(
-    path.resolve("06-build-page", "components")
-  );
+  const componentsPath = path.resolve('06-build-page', 'components');
+  let components = await fs.promises.readdir(componentsPath);
   components = components
-    .map((component) => component.split("."))
-    .filter((component) => component[1] === "html")
+    .map((component) => component.split('.'))
+    .filter((component) => component[1] === 'html')
     .map((component) => component[0]);
 
-    for (let component of components) {
-      if (template.replace(/\s/g, '').includes(`{{${component}}}`)) {
-        let data = await fs.promises.readFile(path.resolve("06-build-page", "components", `${component}.html`));
-        template = template.replace(`{{${component}}}`, data);
-      }
+  for (let component of components) {
+    if (template.replace(/\s/g, '').includes(`{{${component}}}`)) {
+      const componentPath = path.resolve(componentsPath, `${component}.html`);
+      const componentContent = await fs.promises.readFile(componentPath);
+      template = template.replace(`{{${component}}}`, componentContent);
     }
-    fs.open(path.resolve("06-build-page", "project-dist", "index.html"), 'w', (err) => {
-      if (err) throw err;
-      fs.appendFile(path.resolve("06-build-page", "project-dist", "index.html"), template, (err) => {
-        if (err) throw err;
-      })
-    })
-}
-
-// check if style.css exists and remove
-
-fs.access(path.resolve("06-build-page", "project-dist", "style.css"), (err) => {
-  if (!err) {
-    fs.unlink(
-      path.resolve("06-build-page", "project-dist", "style.css"),
-      (err) => {
-        if (err) throw err;
-      }
-    );
   }
-});
+
+  const htmlPath = path.resolve('06-build-page', 'project-dist', 'index.html');
+  await fs.promises.open(htmlPath, 'w');
+  await fs.promises.appendFile(htmlPath, template);
+}
 
 // add file style.css
 
-fs.readdir(path.resolve("06-build-page", "styles"), (err, files) => {
-  if (err) throw err;
-  files = files.filter((file) => file.split(".")[1] === "css");
+async function createCSS() {
+  const stylesPath = path.resolve('06-build-page', 'styles');
+  let files = await fs.promises.readdir(stylesPath);
+  const targetPath = path.resolve('06-build-page', 'project-dist', 'style.css');
+  files = files.filter((file) => file.split('.')[1] === 'css');
   for (let i = 0; i < files.length; i++) {
-    let data = "";
-    const rs = fs.createReadStream(
-      path.resolve("06-build-page", "styles", `${files[i]}`)
+    const filePath = path.resolve(stylesPath, files[i]);
+    const fileContent = await fs.promises.readFile(filePath, 'binary');
+
+    await fs.promises.appendFile(
+      targetPath,
+      i === 0 ? fileContent : '\n' + '\n' + fileContent
     );
-    rs.on("data", (chunk) => {
-      data += chunk.toString();
-    });
-    rs.on("end", () => {
-      fs.appendFile(
-        path.resolve("06-build-page", "project-dist", "style.css"),
-        i === 0 ? data : "\n" + data,
-        (err) => {
-          if (err) throw err;
-        }
-      );
-    });
   }
-});
+}
 
 // remove assets
 
@@ -112,43 +81,41 @@ async function removeFiles(folderPath) {
 // copy assets folder
 
 async function copyDir(sourceFolderPath, targetFolderPath) {
-  createFolder(targetFolderPath);
+  await fs.promises.mkdir(targetFolderPath, { recursive: true });
   const elements = await fs.promises.readdir(sourceFolderPath);
 
   for (let element of elements) {
     const elementSourcePath = path.resolve(sourceFolderPath, element);
     const elementTargetPath = path.resolve(targetFolderPath, element);
-    fs.stat(elementSourcePath, (err, stats) => {
-      if (err) throw err;
-      if (stats.isDirectory()) {
-        createFolder(elementTargetPath);
-        copyDir(elementSourcePath, elementTargetPath);
-      } else {
-        fs.copyFile(elementSourcePath, elementTargetPath, (err) => {
-          if (err) throw err;
-        });
-      }
-    });
+    const stats = await fs.promises.stat(elementSourcePath);
+    if (stats.isDirectory()) {
+      await fs.promises.mkdir(elementTargetPath, { recursive: true });
+      await copyDir(elementSourcePath, elementTargetPath);
+    } else {
+      await fs.promises.copyFile(elementSourcePath, elementTargetPath);
+    }
   }
 }
 
-function createFolder(path) {
-  fs.mkdir(path, { recursive: true }, (err) => {
-    if (err) throw err;
-  });
-}
-
-async function runFunctions() {
-  const assetsPath = path.resolve("06-build-page", "project-dist", "assets");
-  const stat = await fs.promises.stat(assetsPath).catch((_) => null);
+async function removeAssets() {
+  const assetsPath = path.resolve('06-build-page', 'project-dist', 'assets');
+  const stat = await fs.promises.stat(assetsPath).catch(() => null);
   if (stat) {
     await removeFiles(assetsPath);
   }
-  await copyDir(
-    path.resolve("06-build-page", "assets"),
-    path.resolve("06-build-page", "project-dist", "assets")
-  );
+}
+
+async function runFunctions() {
+  await createOutputFolder();
+  await removeFile('index.html');
   await createHTML();
+  await removeFile('style.css');
+  await createCSS();
+  await removeAssets();
+  await copyDir(
+    path.resolve('06-build-page', 'assets'),
+    path.resolve('06-build-page', 'project-dist', 'assets')
+  );
 }
 
 runFunctions();
