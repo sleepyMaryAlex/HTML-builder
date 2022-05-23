6f76f03f6ca0add27a262dde7116895c9eec38fc
Maria Guchkova <mariaguchkova1@gmail.com>
2022-05-23 18:31:27 +0300

    refactor: delete comments

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -30,28 +30,6 @@ async function createBundle() {
   }
 }
 
-// fs.readdir(path.resolve('05-merge-styles', 'styles'), (err, files) => {
-//   if (err) throw err;
-//   files = files.filter((file) => file.split('.')[1] === 'css');
-//   for (let i = 0; i < files.length; i++) {
-//     let data = '';
-//     const rs = fs.createReadStream(
-//       path.resolve('05-merge-styles', 'styles', `${files[i]}`)
-//     );
-//     rs.on('data', (chunk) => {
-//       data += chunk.toString();
-//     });
-//     rs.on('end', () => {
-//       fs.appendFile(
-//         path.resolve('05-merge-styles', 'project-dist', 'bundle.css'),
-//         i === 0 ? data : '\n' + data,
-//         (err) => {
-//           if (err) throw err;
-//         }
-//       );
-//     });
-//   }
-// });
 async function runFunctions() {
   await removeFile();
   await createBundle();
