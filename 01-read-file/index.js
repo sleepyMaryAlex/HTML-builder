4302f82351bed0c1b981ca63386bd2b54956b35b
Maria Guchkova <mariaguchkova1@gmail.com>
2022-05-23 15:06:02 +0300

    refactor: change implementation of first task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-async function createStream() {
-  let data = '';
-  const rs = fs.createReadStream(path.resolve('01-read-file', 'text.txt'));
-  rs.on('data', (chunk) => {
-    data += chunk.toString();
-  });
-  rs.on('end', () => {
-    console.log(data);
-  });
-}
 
-createStream();
+let data = '';
+const rs = fs.ReadStream(path.resolve('01-read-file', 'text.txt'));
+rs.on('data', (chunk) => {
+  data += chunk.toString();
+});
+rs.on('end', () => {
+  console.log(data);
+});
